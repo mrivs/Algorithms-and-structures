0b1df5a10601c60ad8745f5d688d6215d9ee4c88
mrivs <valentinilyins@gmail.com>
2023-06-15 21:03:34 +0300

    task01

--- homework02/homework02.java
+++
@@ -1,9 +0,0 @@
-// Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
-
-package homework02;
-
-public class homework02 {
-    public static void main(String[] args) {
-        
-    }
-}
