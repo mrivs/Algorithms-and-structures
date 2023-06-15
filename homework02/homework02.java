6214fac93bec54048c5b9c8f7a6930847196165b
mrivs <valentinilyins@gmail.com>
2023-06-15 20:57:16 +0300

    initia commit

---
+++ homework02/homework02.java
@@ -0,0 +1,9 @@
+// Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+
+package homework02;
+
+public class homework02 {
+    public static void main(String[] args) {
+        
+    }
+}
